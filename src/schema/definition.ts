4eb54df8c5db35e69c34884f3ae55d15449cb9b8
Toan Nguyen <nta.toan@gmail.com>
2022-02-11 11:45:45 -0500

    chore: fix cicd and resolving path

--- src/schema/definition.ts
+++ src/schema/definition.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const data = fs.readFileSync(path.resolve('./src/schema/schema.graphql'), 'utf8');
+const data = fs.readFileSync(path.resolve('src/schema/schema.graphql'), 'utf8');
 const typeDefs = `
   ${data}
 `;
