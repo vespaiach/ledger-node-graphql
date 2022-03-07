0526ea3a89cd82d5f9ed52ddebeaeecf26b8bb7a
Toan Nguyen <nta.toan@gmail.com>
2022-03-07 15:57:50 -0500

    chore: move type definition into string template

--- src/schema/definition.ts
+++ src/schema/definition.ts
@@ -1,9 +1,45 @@
-import fs from 'fs';
-import path from 'path';
-
-const data = fs.readFileSync(path.resolve('src/schema/schema.graphql'), 'utf8');
 const typeDefs = `
-  ${data}
+  scalar Date
+
+  type Transaction {
+    id: Int!
+    amount: Float!
+    date: Date!
+    note: String
+    updatedAt: Date!
+    reason: Reason!
+  }
+
+  type Reason {
+    id: Int!
+    text: String!
+    updatedAt: Date!
+  }
+
+  type Query {
+    getReasons: [Reason!]
+    getTransaction(id: Int!): Transaction
+    getTransactions(
+      fromDate: Date
+      toDate: Date
+      fromAmount: Int
+      toAmount: Int
+      reasonIds: [Int!]
+      lastCursor: Int
+      take: Int = 50
+    ): [Transaction!]
+  }
+
+  type Mutation {
+    createReason(text: String!): Reason
+    updateReason(id: Int!, text: String!): Reason
+    deleteReason(id: Int!): Boolean
+
+    createTransaction(date: Date!, amount: Float!, reasonId: Int!, note: String): Transaction
+    updateTransaction(id: Int!, date: Date, amount: Float, reasonId: Int, note: String): Transaction
+    deleteTransaction(id: Int!): Boolean
+  }
+
 `;
 
 export { typeDefs };
